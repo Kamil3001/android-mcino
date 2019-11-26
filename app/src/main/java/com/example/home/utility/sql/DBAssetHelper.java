d04cd94562d7baa420b65d11689937053ee236d9
Daniel139 <Daniel139@users.noreply.github.com>
2019-11-26 10:09:12 +0000

    Added comments and tidied up

--- app/src/main/java/com/example/home/utility/sql/DBAssetHelper.java
+++ app/src/main/java/com/example/home/utility/sql/DBAssetHelper.java
@@ -9,69 +9,96 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 
+/***
+ * This database helper class is used to access the database (assets/databases/county_details.db)
+ * with tables: county, county_colours, figures & services by
+ *
+ *      ContactsFragment.java: to get homeless service contact details per county in the Republic of Ireland.
+ *      tables: county, services
+ *
+ *      StatisticsFragments.java to retrieve the name of a county from its colour selected in the coloured map of
+ *      Ireland then return the corresponding homeless figures for that county.
+ *      tables: county_colours, figures
+ * */
 public class DBAssetHelper extends SQLiteAssetHelper {
 
-    private static final String DATABASE_NAME = "county_details";
     private static final int DATABASE_VERSION = 10;
-    private static SQLiteDatabase db;
-    private static SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-    private String currentCounty;
-    private Cursor cursor;
+    private static final String DATABASE_NAME = "county_details.db";
 
-    private static Cursor allServices;
-
-
+    // SQLite commands
     private String [] SELECT = {"*"};
-    private String TABLES = "county, services";
     private String INNER_JOIN = "services INNER JOIN county ON (services.County = county.County_id)";
-    private String WHERE_DUBLIN = "services.County='FING' OR services.County='SOUDUB' OR services.County='DUBCITY' OR services.County='DUNRATH'";
-    private String WHERE_COUNTY = "county_name = '";
     private String SORT_COUNTY = "services.County";
 
+    private static SQLiteDatabase db;
+    private static SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+    private Cursor cursor;
+    private String currentCounty;   // last county selected in statistics
+
+
+    /**Constructor calls base class constructor then instantiates db **/
     public DBAssetHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         db = getReadableDatabase();
-        setAllCounties();
     }
 
-
+    /**@param userCounty a county selected by the user from a spinner or set by location
+     *                    default spinner value "Select county…" will show all counties
+     * @return Cursor containing table information of selected county/counties **/
     public Cursor getColumns(String userCounty) {
-
         qb = new SQLiteQueryBuilder();
 
-        String sqlWhere = WHERE_COUNTY+userCounty+"'";
-
-        // Initially show all counties on startup - can change to use current location
         if(userCounty.equals("Select county…")){
-            allServices.moveToFirst();
-            return allServices;
-
+            setAllCounties();
         }
         else if(userCounty.equals("Dublin")){
-            qb.setTables(INNER_JOIN);
-            cursor = qb.query(db, SELECT,WHERE_DUBLIN, null, null, null, SORT_COUNTY);
+            setDublin();
         }
         else {
-
-            qb.setTables(TABLES);
-
-            cursor = qb.query(db, SELECT, sqlWhere, null, null, null, null);
-            cursor.moveToFirst();
-
-            qb.setTables(INNER_JOIN);
-            sqlWhere = "County_id = '" + cursor.getString(cursor.getColumnIndex("County_id")) + "'";
-
-            cursor = qb.query(db, SELECT, sqlWhere, null, null, null, SORT_COUNTY);
+            setCounty(userCounty);
         }
         cursor.moveToFirst();
         return cursor;
     }
 
+    /** Assign cursor the set of services for all counties in descending order **/
     private void setAllCounties(){
         qb.setTables(INNER_JOIN);
-        allServices = qb.query(db, SELECT, null, null, null, null, SORT_COUNTY);
-        allServices.moveToFirst();
+        cursor = qb.query(db, SELECT, null, null, null, null, SORT_COUNTY);
     }
+    /** Assign cursor the set of services for the 4 Dublin sub-regions
+     * Dublin City, Dún Laoghaire, Fingal & South Dublin
+     * in descending order **/
+    private void setDublin(){
+        String WHERE_DUBLIN = "services.County='FING' OR services.County='SOUDUB' OR services.County='DUBCITY' OR services.County='DUNRATH'";
+
+        qb.setTables(INNER_JOIN);
+        cursor = qb.query(db, SELECT, WHERE_DUBLIN, null, null, null, SORT_COUNTY);
+    }
+    /**
+     * The county id (county.County_id) is first obtained from the county table to be used to get all services for
+     * @param county
+     * from the services table which are assigned to cursor
+     *
+     * **/
+    private void setCounty(String county){
+        String sqlWhere = "county_name = '" +county+"'";
+        String TABLES = "county";
+
+        qb.setTables(TABLES);
+        cursor = qb.query(db, SELECT, sqlWhere, null, null, null, null);
+        cursor.moveToFirst();
+
+        sqlWhere = "County_id = '" + cursor.getString(cursor.getColumnIndex("County_id")) + "'";
+        qb.setTables(INNER_JOIN);
+        cursor = qb.query(db, SELECT, sqlWhere, null, null, null, SORT_COUNTY);
+    }
+
+    /** This method queries the county_colours table for the corresponding county by colour.
+     * @param hexValue contains the hexadecimal digits corresponding to a colour
+     * @return String containing the name of the county colour with hexValue
+     * If white or black are passed (regions outside map) or no examples of hexValue are in
+     * county_colours we return the previously selected county**/
     public String getCountyByColour(String hexValue){
         if(!hexValue.equals("000000") && !hexValue.equals("ffffff")) {
             qb.setTables("county_colours");
@@ -85,6 +112,9 @@ public class DBAssetHelper extends SQLiteAssetHelper {
         }
         return currentCounty;
     }
+    /** For a given county we return the homeless figures from table figures
+     * @param county county to get homeless figures for
+     * @return cursor containing table of homeless figures**/
     public Cursor getCountyFigures(String county){
         qb.setTables("figures");
         String WHERE = "figures.County='" + county +"'";
